4823c5d641726a3384075322ab026a05798f1aaa
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2025-04-28 20:38:38 -0400

    hello

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var board1 = Chessboard('board1', {
   position: 'start',
   draggable: true,
-  pieceTheme: 'chessboardjs-1.0.0/img/chesspieces/wikipedia/{piece}.png',
-  position: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+  pieceTheme: 'chessboardjs-1.0.0/img/chesspieces/wikipedia/{piece}.png'
 });
