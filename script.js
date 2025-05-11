c89fef7f683c496676bc010a7558807b4fe7cf1a
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2025-05-11 12:09:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var board1 = Chessboard('board1', {
-  draggable: false,
-  pieceTheme: 'chessboardjs-1.0.0/img/chesspieces/wikipedia/{piece}.png'
-});
 
-function fetchFENAndUpdate() {
-  $.get('latest_fen.txt', function (data) {
-    board1.position(data.trim());
-  });
+var board = Chessboard('myBoard', 'start');  // Initially set to 'start'
+
+function updateBoard() {
+  fetch('http://bogdanpi.local:8080/get_fen')
+    .then(response => response.text())
+    .then(fenString => {
+      board.position(fenString);  // Update board with the new FEN string
+    })
+    .catch(error => console.error('Error fetching FEN:', error));
 }
+
+// You can call updateBoard periodically or based on an event to update the chessboard
+setInterval(updateBoard, 1000);  // Update board every second
+
 
 // Poll every 2 seconds
 setInterval(fetchFEN, 2000);
+
