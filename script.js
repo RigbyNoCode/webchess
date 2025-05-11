d8036023c5bc034e505e2d2fe88fb31bb88404a9
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2025-05-11 11:54:55 -0400

    Pls

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var board1 = Chessboard('board1', {
   draggable: false,
   pieceTheme: 'chessboardjs-1.0.0/img/chesspieces/wikipedia/{piece}.png'
 });
+
+function fetchFEN() {
+  fetch('fen.txt') // this file should contain only the FEN string
+    .then(response => response.text())
+    .then(fen => {
+      board1.position(fen.trim());
+    })
+    .catch(err => console.error('Error fetching FEN:', err));
+}
+
+// Poll every 2 seconds
+setInterval(fetchFEN, 2000);
