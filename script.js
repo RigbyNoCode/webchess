280244a96ced221696221adc9a45a22745b732ff
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2025-05-11 11:59:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ var board1 = Chessboard('board1', {
   pieceTheme: 'chessboardjs-1.0.0/img/chesspieces/wikipedia/{piece}.png'
 });
 
-function fetchFEN() {
-  fetch('fen.txt') // this file should contain only the FEN string
-    .then(response => response.text())
-    .then(fen => {
-      board1.position(fen.trim());
-    })
-    .catch(err => console.error('Error fetching FEN:', err));
+function fetchFENAndUpdate() {
+  $.get('latest_fen.txt', function (data) {
+    board1.position(data.trim());
+  });
 }
 
 // Poll every 2 seconds
