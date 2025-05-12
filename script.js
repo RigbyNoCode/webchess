5aa6bd43d9326ff438cf686b56c9f24bf7052ccf
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2025-05-11 21:11:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
- var board = Chessboard('myBoard', 'fen');
+ var board = Chessboard('myBoard', 'start');
 
   function fetchFEN() {
     fetch('fen.txt')
       .then(response => response.text())
       .then(fenString => {
-        board.position(fenString);
-      })
-      .catch(error => console.error('Error fetching FEN:', error));
+        var fen = fenString.trim();
+        if (fen) {
+          board.position(fen);
+        } else {
+          console.error('FEN string is empty or invalid');
+        }
   }
+}
 
   fetchFEN();
   setInterval(fetchFEN, 2000);
